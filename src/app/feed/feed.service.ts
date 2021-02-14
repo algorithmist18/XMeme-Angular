78d6585a88b5fce511f1c9b619c2056101559d7b
Avinash Mishra <avinashm2427@gmail.com>
2021-02-14 11:11:45 +0530

    Added public URL for backend calls

--- src/app/feed/feed.service.ts
+++ src/app/feed/feed.service.ts
@@ -7,20 +7,20 @@ import { HttpClient } from '@angular/common/http';
 })
 export class FeedService {
 
-  URL_PREFIX: string = 'http://localhost:'; 
+  URL_PREFIX: string = 'https://afternoon-coast-72955.herokuapp.com/'
   PORT: number = 8080; 
 
   constructor(private httpClient: HttpClient) { }
 
   fetchMemes(): Observable <any> {
 
-    let url: string = this.URL_PREFIX + this.PORT + '/memes'; 
+    let url: string = this.URL_PREFIX + '/memes'; 
     return this.httpClient.get(url); 
   }
 
   patchMeme(id: number, form: any): Observable <any> {
 
-    let url: string = this.URL_PREFIX + this.PORT + '/memes/' + id; 
+    let url: string = this.URL_PREFIX + '/memes/' + id; 
     return this.httpClient.patch(url, form.value); 
   }
 }
--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -7,14 +7,14 @@ import { Observable } from 'rxjs';
 })
 export class HomeService {
 
-  URL_PREFIX: string = 'http://localhost:'; 
+  URL_PREFIX: string = 'https://afternoon-coast-72955.herokuapp.com/'; 
   PORT: number = 8080; 
 
   constructor(private httpClient: HttpClient) { }
 
   postMeme(data: any): Observable <any> {
 
-    let url: string = this.URL_PREFIX + this.PORT + '/memes'; 
+    let url: string = this.URL_PREFIX + '/memes'; 
     console.log(url); 
     return this.httpClient.post(url, data.value); 
     
